938d9a5ac04d6329883d16892597e72060d1ca59
nafhul <nafhul.arsyad@gmail.com>
2019-10-29 09:31:29 +0700

    fix(go-vet): adjust command

--- run-go-vet.sh
+++ run-go-vet.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
-pkg=$(go list)
-for dir in $(echo $@|xargs -n1 dirname|sort -u); do
-  go vet $pkg/$dir
-done
+#pkg=$(go list)
+#for dir in $(echo $ @|xargs -n1 dirname|sort -u); do
+#  go vet $pkg/$dir
+#done
+go vet ./...
